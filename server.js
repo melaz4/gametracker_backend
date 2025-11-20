a960fbfa054511ecd6d5f770151a7112a0282222
Sebastian Reyes <reyesmontessebastian03@gmail.com>
2025-11-19 22:35:20 -0500

    boton para eliminar

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(MONGODB_URI)
   .then(() => console.log('Conectado a MongoDB atlas'))
   .catch(err => console.error('Error conectando a MongoDB:', err));
 
-  
+
 //espacio de endpoints
 
 //endpoint para agregar un nuevo juego
@@ -75,16 +75,18 @@ app.put('/api/juegos/:id', async (req, res) => {
 
 //endpoint para elimina un juego
 app.delete('/api/juegos/:id', async (req, res) => {
-    try {
-        const juegoEliminado = await Juego.findByIdAndDelete(req.params.id);
+	try {
+		const juegoEliminado = await Juego.findByIdAndDelete(req.params.id);
 
-        if (!juegoEliminado) return res.status(404).json({ error: 'Juego no encontrado' });
+		if (!juegoEliminado)
+			return res.status(404).json({ error: 'Juego no encontrado' });
 
-        res.json({ mensaje: 'Juego eliminado correctamente' });
-    } catch (error) {
-        res.status(400).json({ error: 'Error al eliminar el juego' });
-    }
+		res.json({ mensaje: 'Juego eliminado correctamente' });
+	} catch (error) {
+		res.status(400).json({ error: 'Error al eliminar el juego' });
+	}
 });
+
 
 
 const PORT = 5000;
